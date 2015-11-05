009ac5e25aee6775de3c6bf737d6df043c38f394
ganaGitHub <ganeswara.n.rao@verizon.com>
2015-11-04 20:14:42 -0800

    Changed Image

--- src/com/verizon/JITDispatch/login/action/LoginAction.java
+++ src/com/verizon/JITDispatch/login/action/LoginAction.java
@@ -38,6 +38,22 @@ public class LoginAction  extends HttpServlet {
     	
     }
     
+/*	protected void initialize(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = null;
+		try {
+			System.out.println("Redirecting the request to create request page");
+			
+			rd = request.getRequestDispatcher("jsp/login/login.jsp");
+			// rd = request.getRequestDispatcher("jsp/common/home.jsp");
+			rd.forward(request, response);
+		} catch (Exception e) {
+			System.out.println("Exception occurred while redirecting the request to create request page, exception: " + e.toString());
+			System.out.println(e);
+			rd = request.getRequestDispatcher("jsp/common/error.jsp?error=Error occurred while redirecting the request to create request page!");
+			rd.forward(request, response);
+		}
+	}  End of initialize 
+*/	
     protected void initialize(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher rd = null;
 		try {
@@ -53,6 +69,62 @@ public class LoginAction  extends HttpServlet {
 		}
 	} /* End of initialize */
     
+/*	protected void login(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    	RequestDispatcher rd = null;
+    	HttpSession session = request.getSession();
+		
+		try {
+    		String userName = (String) request.getParameter("userName");
+    		userName = "HydUser";
+			System.out.println("userName: " + userName);
+			session.setAttribute("sUserName", userName);
+			
+			// Reset the issue if logging again
+			session.removeAttribute("sIssueName");
+			
+			String password = (String) request.getParameter("password");
+			System.out.println("password: " + password);
+			
+			LoginModel loginModelObj = new LoginModel();
+//			AuthorizationModel authorizationModelObj = new AuthorizationModel();
+//			ArrayList<UserDetailsBean> userDetailsBeanAL = new ArrayList<UserDetailsBean>();
+//			
+//			userDetailsBeanAL = loginModelObj.loadUserDetails();
+//			String originalPassword = authorizationModelObj.getPassword(userName, userDetailsBeanAL);
+//			System.out.println("originalPassword: " + originalPassword);
+//    			
+//			String [] groupRole = new String[2];
+			if(userName.equals(userName))
+    		{
+				System.out.println("Login success for user");
+				
+    			rd = request.getRequestDispatcher("jsp/common/home.jsp");
+				rd.forward(request, response);
+    		}
+    		else if(password == "")
+    		{
+    			System.out.println("User does not have an account on this application, please check with admin");
+    			rd = request.getRequestDispatcher("jsp/login/login.jsp?status=User does not have an account on this application, please check with admin!");
+				rd.forward(request, response);
+    		}
+    		else 
+    		{
+    			System.out.println("Invalid user name or password, please try again!");
+    			rd = request.getRequestDispatcher("jsp/login/login.jsp?status=Invalid user name or password, please try again!");
+				rd.forward(request, response);
+    		}			
+		}
+    	catch(Exception e)
+        {
+            System.out.println("Exception occurred while login, exception: " + e.toString());
+            System.out.println(e);
+            rd = request.getRequestDispatcher("jsp/login/login.jsp?status=Error occurred while login, please try again!");
+            rd.forward(request, response);        
+        }
+    }
+*/
+
 	protected void login(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
     	RequestDispatcher rd = null;
@@ -60,7 +132,8 @@ public class LoginAction  extends HttpServlet {
 		
 		try {
     		String userName = (String) request.getParameter("userName");
-    		System.out.println("userName: " + userName);
+    		// userName = "HydUser" and "ChnUser";
+			System.out.println("userName: " + userName);
 			session.setAttribute("sUserName", userName);
 			
 			// Reset the issue if logging again
@@ -75,6 +148,7 @@ public class LoginAction  extends HttpServlet {
 				System.out.println("Redirecting to customer: " + userName + " page");
 				
 				rd = request.getRequestDispatcher("CreateRequestAction?param=init");
+    			// rd = request.getRequestDispatcher("jsp/common/home.jsp");
 				rd.forward(request, response);
     		}
 			else 
